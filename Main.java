c1e6b0e8541335434688e16efd922b0d30617a62
heitorrodriguesferreira1411-max <heitorrodriguesferreira1411@gmail.com>
2026-05-09 11:58:52 -0300

    jk
    hk

---
+++ Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	int codigo;
+		
+	Scanner leia= new Scanner(System.in);
+	
+	System.out.println("OS produtos são:");
+	System.out.println("Abacaxi- codigo 1 ");
+	System.out.println("Goiaba- codigo 2 ");
+    System.out.println("Laranja- codigo 3 ");
+	System.out.println("Digite o codigo do produto de sua preferencia");
+	codigo = leia.nextInt();
+	
+    if (codigo = "1")
+	{
+	    System.out.println("o produto custa:8,9");
+	}
+	  if (codigo = "2")
+	{
+	    System.out.println("o produto custa:2,0");
+	}
+	  if (codigo = "3")
+	{
+	    System.out.println("o produto custa:1,8");
+	}
+	
+	
+	
+	
+	
+	
+	}
+}

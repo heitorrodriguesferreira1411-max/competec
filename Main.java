dcb30674aed3d544ef74a238ce7bc44fa2e8ce00
heitorrodriguesferreira1411-max <heitorrodriguesferreira1411@gmail.com>
2026-06-13 11:22:57 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,34 +2,14 @@ import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-	int codigo;
+	Scanner leia = new Scanner(System.in);
+	int n1;
 		
-	Scanner leia= new Scanner(System.in);
-	
-	System.out.println("OS produtos são:");
-	System.out.println("Abacaxi- codigo 1 ");
-	System.out.println("Goiaba- codigo 2 ");
-    System.out.println("Laranja- codigo 3 ");
-	System.out.println("Digite o codigo do produto de sua preferencia");
-	codigo = leia.nextInt();
-	
-    if (codigo = "1")
-	{
-	    System.out.println("o produto custa:8,9");
-	}
-	  if (codigo = "2")
-	{
-	    System.out.println("o produto custa:2,0");
-	}
-	  if (codigo = "3")
-	{
-	    System.out.println("o produto custa:1,8");
-	}
-	
-	
-	
-	
-	
-	
+	System.out.println("Digite um numero");
+	n1 = leia.nextInt();
+	    
+	int n2[] = new int [n1];    
+	    
+	System.out.println(n2);    
 	}
 }

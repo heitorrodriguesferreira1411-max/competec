416dea7255e213afea04bd23e43e218fff47351b
heitorrodriguesferreira1411-max <heitorrodriguesferreira1411@gmail.com>
2026-06-13 11:25:32 -0300

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.Arrays; 
+
+public class Main
+{
+	public static void main(String[] args) {
+     int n1 [] = {1,2,3};
+     int soma;
+     
+     soma = n1[0] + n1[1] + n1[2];
+		
+		System.out.println(soma);
+		
+		
+	}
+}
